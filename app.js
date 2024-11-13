6d131a13b3ede90c92203b4fecf9c8b93216b8d0
henrycharles <henrycharles2544@gmail.com>
2024-11-13 17:07:57 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const b= 1+3
+console.log(b);
+
+let animalls=[{name:"cow",age:20 ,colour:"white"},
+             {name:"goat",age:10 ,colour:"black"},
+             {name:"hen",age:1 ,colour:"brown"},
+             {name:"dog",age:3 ,colour:"grey"}];
+
+             console.log(animalls);
